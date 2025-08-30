4085f3e7271def55c8a3107308516687e7926997
andresroffe0 <124309850+andresroffe0@users.noreply.github.com>
2025-08-29 18:18:48 -0600

    Add files via upload

---
+++ api_sie.py
@@ -0,0 +1,6 @@
+def banxico(token, series, fechaIni, fechaFin):
+    url = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{series}/datos/{fechaIni}/{fechaFin}'
+    headers = {'Bmx-Token': token}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data

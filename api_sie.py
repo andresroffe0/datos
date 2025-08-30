af842bf290825bcd585bc5e656ec3c5d91a923ea
andresroffe0 <124309850+andresroffe0@users.noreply.github.com>
2025-08-30 01:47:42 -0600

    Update api_sie.py
    Agregué la importación de requests

--- api_sie.py
+++ api_sie.py
@@ -1,3 +1,4 @@
+import requests
 def banxico(token, series, fechaIni, fechaFin):
     url = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{series}/datos/{fechaIni}/{fechaFin}'
     headers = {'Bmx-Token': token}
